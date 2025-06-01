b7dbcf8ca0c1ce63c18fa3d06b7441efacc57d02
Clara Pelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-06-01 18:20:23 -0300

    feat-design: agregue fuente MONTSERRAT

--- src/pdf/styles/pdfstyles.jsx
+++ src/pdf/styles/pdfstyles.jsx
@@ -1,21 +1,55 @@
 import { StyleSheet } from '@react-pdf/renderer';
 
 export default StyleSheet.create({
-  page: { padding: 30 },
+  page: { padding: 30, fontFamily: 'Montserrat' },
   section: { marginBottom: 20 },
-  title: { fontSize: 14, marginBottom: 10, fontWeight: 'bold' },
-  titleWithBackground: {
-    fontSize: 14, marginBottom: 10, fontWeight: 'bold',
-    backgroundColor: '#264653', color: '#fff', padding: 6,
+  title: {
+    fontSize: 14,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat'
   },
-  smallTitle: { fontSize: 10, marginBottom: 8, fontWeight: 'bold' },
+  titleWithBackground: {
+    fontSize: 14,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    backgroundColor: '#264653',
+    color: '#fff',
+    padding: 6,
+    fontFamily: 'Montserrat'
+  },
+  smallTitle: {
+    fontSize: 10,
+    marginBottom: 8,
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat'
+  },
   row: { flexDirection: 'row' },
-  cell: { flex: 1, fontSize: 10, padding: 4, borderWidth: 0.5, borderColor: '#000000' },
-  cellTotal: { fontSize: 12, padding: 4, borderWidth: 0.5, borderColor: '#000000' },
+  cell: {
+    flex: 1,
+    fontSize: 10,
+    padding: 4,
+    borderWidth: 0.5,
+    borderColor: '#000000',
+    fontFamily: 'Montserrat'
+  },
+  cellTotal: {
+    fontSize: 12,
+    padding: 4,
+    borderWidth: 0.5,
+    borderColor: '#000000',
+    fontFamily: 'Montserrat'
+  },
   headerCell: {
-    flex: 1, fontSize: 10, padding: 4, fontWeight: 'bold',
-    borderWidth: 0.5, borderColor: '#000000',
-    backgroundColor: '#264653', color: '#FFFFFF'
+    flex: 1,
+    fontSize: 10,
+    padding: 4,
+    fontWeight: 'bold',
+    borderWidth: 0.5,
+    borderColor: '#000000',
+    backgroundColor: '#264653',
+    color: '#FFFFFF',
+    fontFamily: 'Montserrat'
   },
   topInfoContainer: { marginBottom: 20 },
   image: { width: '100%', height: 200, marginBottom: 20 }
