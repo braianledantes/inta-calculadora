34edb9b7e20d5d0a69d7b7a04203da716853bff9
Clara Pelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-06-01 18:28:35 -0300

    cambie un value

--- src/components/CardMaquinariaPlan/CardMaquinariaPlan.jsx
+++ src/components/CardMaquinariaPlan/CardMaquinariaPlan.jsx
@@ -43,7 +43,7 @@ export function CardMaquinariaPlan({plan, tractores, implementos, onUpdate, onDe
           />
           <NumberValue name="Potencia" value={plan.tractor.potencia} unit="HP" />
           <NumberValue name="Precio" value={plan.tractor.precioDolar} unit="US$" />
-          <NumberValue name="Coef. conservación" value={plan.tractor.gastoMantenimiento} />
+          <NumberValue name="Coef. conserv." value={plan.tractor.gastoMantenimiento} />
           <NumberValue name="Horas útiles" value={plan.tractor.horasVidaUtil} unit="h" />
           <NumberValue name="Valor residual" value={plan.tractor.porcentajeValorResidual} unit="%" />
         </div>
@@ -73,7 +73,7 @@ export function CardMaquinariaPlan({plan, tractores, implementos, onUpdate, onDe
           />
           <NumberValue name="Consumo" value={plan.implemento.consumoCombustible} unit="lt/h" />
           <NumberValue name="Precio" value={plan.implemento.precioDolar} unit="US$" />
-          <NumberValue name="Coef. conservación" value={plan.implemento.gastoMantenimiento} />
+          <NumberValue name="Coef. conserv." value={plan.implemento.gastoMantenimiento} />
           <NumberValue name="Horas útiles" value={plan.implemento.horasVidaUtil} unit="h" />
           <NumberValue name="Valor residual" value={plan.implemento.porcentajeValorResidual} unit="%" />
         </div>
