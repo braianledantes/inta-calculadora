612e5d9737dcf30b18097e333daa438e673cae5a
Clara Pelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-06-02 16:17:46 -0300

    re-desing: menu cards

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
   return (
     <>
       {/* Header */}
-      <header className="sticky top-0 z-30 bg-[#eeeeee] border-b-1 p-4">
+      <header className="sticky top-0 z-30 bg-gradient-to-b from-white to-[#ccc] border-b-1 ">
         <div className="w-full flex flex-col md:flex-row items-center justify-between gap-2 px-4 py-2">
 
           {/* Logo & Hamburger */}
@@ -39,13 +39,13 @@ const Header = () => {
               <img
                 src={sipan}
                 alt="Sipan Logo"
-                className="h-[35px] md:h-[54px] cursor-pointer"
+                className="h-[25px] md:h-[54px] cursor-pointer"
               />
             </Link>
           </div>
           <div className="absolute top-0 right-0 h-full hidden md:flex items-center justify-center px-6">
-            <div className="h-full flex items-center justify-center px-4 border-l border-[#ccc] transition-all duration-300 group hover:bg-gradient-to-b hover:from-orange-500 hover:to-orange-600 cursor-pointer">
-              <span className="text-black text-lg transition-all duration-300 group-hover:text-white">Calculadoras</span>
+            <div className="h-full flex items-center justify-center px-4 border-l border-[#ccc] transition-all duration-300 group bg-gradient-to-b from-orange-500 to-orange-600 cursor-pointer">
+              <span className="text-white text-lg transition-all duration-300 ">Calculadoras</span>
             </div>
           </div>
 
@@ -53,7 +53,7 @@ const Header = () => {
       </header>
 
       {/* Cards Section */}
-      <div className="bg-[#eeeeee] shadow-2xl rounded-2xl">
+      <div className="bg-gray-100 pt-12">
         <div className="flex flex-wrap justify-center gap-3">
           {cards.map(({ path, label, bg }) => (
             <Link key={path} to={path} onClick={() => setOpen(false)}>
@@ -62,16 +62,16 @@ const Header = () => {
                   group w-[300px] aspect-[16/9] m-2 flex items-end bg-cover bg-center text-black cursor-pointer
                   transform transition duration-300 ease-in-out rounded-2xl 
                   hover:shadow-xl hover:scale-[1.02]
-                  ${location.pathname === path ? "shadow-2xl scale-[1.01] border-1 border-gray-600" : ""}
+                  ${location.pathname === path ? "shadow-2xl scale-[1.01]" : ""}
                 `}
                 style={{ backgroundImage: `url("${bg}")` }}
               >
                 {location.pathname === path ? (
-                  <div className="w-full backdrop-blur-md bg-white/60 text-4xl text-center py-2 rounded-b-2xl">
+                  <div className="w-full backdrop-blur-md bg-orange-500/60 text-3xl text-center py-2 rounded-b-2xl text-white">
                     {label}
                   </div>
                 ) : (
-                  <div className="w-full backdrop-blur-md bg-white/50 text-4xl text-center py-2 rounded-b-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <div className="w-full backdrop-blur-md bg-orange-500/20 text-3xl text-center text-white py-2 rounded-b-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     {label}
                   </div>
                 )}
@@ -79,7 +79,7 @@ const Header = () => {
             </Link>
           ))}
         </div>
-          <div className="p-4 flex justify-center">
+          <div className="py-4 flex justify-center">
             <ImportExcel />
           </div>
       </div>
