6ba9d7932882c134e8ffdc02658054f5ee26d156
Clara Pelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-06-02 14:55:15 -0300

    fix-error: fonts en pdf - nulo

--- src/pdf/styles/pdfstyles.jsx
+++ src/pdf/styles/pdfstyles.jsx
@@ -1,13 +1,12 @@
 import { StyleSheet } from '@react-pdf/renderer';
 
 export default StyleSheet.create({
-  page: { padding: 30, fontFamily: 'Montserrat' },
+  page: { padding: 30 },
   section: { marginBottom: 20 },
   title: {
     fontSize: 14,
     marginBottom: 10,
     fontWeight: 'bold',
-    fontFamily: 'Montserrat'
   },
   titleWithBackground: {
     fontSize: 14,
@@ -16,13 +15,11 @@ export default StyleSheet.create({
     backgroundColor: '#264653',
     color: '#fff',
     padding: 6,
-    fontFamily: 'Montserrat'
   },
   smallTitle: {
     fontSize: 10,
     marginBottom: 8,
     fontWeight: 'bold',
-    fontFamily: 'Montserrat'
   },
   row: { flexDirection: 'row' },
   cell: {
@@ -31,14 +28,12 @@ export default StyleSheet.create({
     padding: 4,
     borderWidth: 0.5,
     borderColor: '#000000',
-    fontFamily: 'Montserrat'
   },
   cellTotal: {
     fontSize: 12,
     padding: 4,
     borderWidth: 0.5,
     borderColor: '#000000',
-    fontFamily: 'Montserrat'
   },
   headerCell: {
     flex: 1,
@@ -49,7 +44,6 @@ export default StyleSheet.create({
     borderColor: '#000000',
     backgroundColor: '#264653',
     color: '#FFFFFF',
-    fontFamily: 'Montserrat'
   },
   topInfoContainer: { marginBottom: 20 },
   image: { width: '100%', height: 200, marginBottom: 20 }
