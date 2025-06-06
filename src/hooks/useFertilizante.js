20a0cc30df77a972dd664b3001f159357d4af9f5
Diiegorios46 <diegoavilaaa0@gmail.com.com>
2025-06-05 22:47:46 -0300

    fix: remove extraneous character in updatePlan function

--- src/hooks/useFertilizante.js
+++ src/hooks/useFertilizante.js
@@ -43,7 +43,7 @@ export const useFertilizante = () => {
     setPlanes([...planes, newPlan]);
   }
 
-  const updatePlan = (id, fertilizante, cantTratamientos) => {j
+  const updatePlan = (id, fertilizante, cantTratamientos) => {
     const valoresCalculados = calcularValoresPlanFertilizante(fertilizante, cantTratamientos, valorDolar);
 
     const updatedPlan = {
