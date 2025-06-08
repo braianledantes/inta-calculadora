2df518f94b6deed6baab73853ff32a01e50ab09c
Clara Pelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-06-08 02:12:20 -0300

    feat: se agregaron los calculos de fertilizantes y sanitizantes!!

--- src/utils/exportExcel.js
+++ src/utils/exportExcel.js
@@ -16,132 +16,182 @@ export function exportFormToExcel(
 
   const workbook = XLSX.utils.book_new();
 
-  // Filas de Maquinaria > index + 1 = Tractor > index + 2 = Implemento
-  const maquinariaRows = [];
-  dataMaquinaria.forEach((plan, index) => {
-    maquinariaRows.push({
-      'Plan': `Plan ${index + 1}`,
-      'Implemento': 'Tractor',
-      'Potencia': plan.tractor?.potencia || '',
-      'Precio dólar': plan.tractor?.precioDolar || '',
-      'Gasto conservación Coeficiente': plan.tractor?.gastoMantenimiento || '',
-      'horas utiles': plan.tractor?.horasVidaUtil || '',
-      'Valor residual %precio': plan.tractor?.porcentajeValorResidual || '',
-      'Consumo combustible lt/hora CV': '',
-      'Amortización $/hora': '', 
-      'Costo Combustible $/hora': '',
-      'Gasto conservación $/hora': '',
-      'Costo Económico $/hora': ''
+  // Maquinaria
+  if (dataMaquinaria.length > 0) {
+    const maquinariaRows = [];
+    dataMaquinaria.forEach((plan, index) => {
+      maquinariaRows.push({
+        'Plan': `${index + 1}`,
+        'Implemento': 'Tractor',
+        'Potencia': plan.tractor?.potencia || '',
+        'Precio dólar': plan.tractor?.precioDolar || '',
+        'Gasto conservación Coeficiente': plan.tractor?.gastoMantenimiento || '',
+        'horas utiles': plan.tractor?.horasVidaUtil || '',
+        'Valor residual %precio': plan.tractor?.porcentajeValorResidual || '',
+        'Consumo combustible lt/hora CV': '',
+        'Amortización $/hora': '', 
+        'Costo Combustible $/hora': '',
+        'Gasto conservación $/hora': '',
+        'Costo Económico $/hora': ''
+      });
+
+      maquinariaRows.push({
+        'Plan': '',
+        'Implemento': plan.implemento?.nombre || '',
+        'Potencia': '',
+        'Precio dólar': plan.implemento?.precioDolar || '',
+        'Gasto conservación Coeficiente': plan.implemento?.gastoMantenimiento || '',
+        'horas utiles': plan.implemento?.horasVidaUtil || '',
+        'Valor residual %precio': plan.implemento?.porcentajeValorResidual || '',
+        'Consumo combustible lt/hora CV': plan.implemento?.consumoCombustible || '',
+        'Amortización $/hora': '', 
+        'Costo Combustible $/hora': '', 
+        'Gasto conservación $/hora': '',
+        'Costo Económico $/hora': ''  
+      });
     });
 
-    maquinariaRows.push({
-      'Plan': '',
-      'Implemento': plan.implemento?.nombre || '',
-      'Potencia': '',
-      'Precio dólar': plan.implemento?.precioDolar || '',
-      'Gasto conservación Coeficiente': plan.implemento?.gastoMantenimiento || '',
-      'horas utiles': plan.implemento?.horasVidaUtil || '',
-      'Valor residual %precio': plan.implemento?.porcentajeValorResidual || '',
-      'Consumo combustible lt/hora CV': plan.implemento?.consumoCombustible || '',
-      'Amortización $/hora': '', 
-      'Costo Combustible $/hora': '', 
-      'Gasto conservación $/hora': '',
-      'Costo Económico $/hora': ''  
+    const wsMaquinaria = XLSX.utils.json_to_sheet(maquinariaRows, { origin: 1 });
+
+    wsMaquinaria[`B1`] = { v: 'Gasoil $/litro' };
+    wsMaquinaria[`C1`] = { v: valorGasoilina };
+    wsMaquinaria[`D1`] = { v: 'cotización dólar' };
+    wsMaquinaria[`E1`] = { v: valorDolar };
+
+    const range = XLSX.utils.decode_range(wsMaquinaria['!ref']);
+    range.s.r = 0;
+    wsMaquinaria['!ref'] = XLSX.utils.encode_range(range);
+
+    dataMaquinaria.forEach((plan, index) => {
+      const excelRowIndex = index * 2 + 3;
+      const rowIndex = excelRowIndex - 1;
+
+      const cellPotencia = `C${excelRowIndex}`;
+      const cellPrecioDolar = `D${excelRowIndex}`;
+      const cellCoeficiente = `E${excelRowIndex}`;
+      const cellHorasUtiles = `F${excelRowIndex}`;
+      const cellValorResidual = `G${excelRowIndex}`;
+      const cellAmortizacion = XLSX.utils.encode_cell({ c: 8, r: rowIndex });
+      const cellGastoConservacion = XLSX.utils.encode_cell({ c: 10, r: rowIndex });
+
+      wsMaquinaria[cellAmortizacion] = { f: `+((${cellPrecioDolar} - (${cellPrecioDolar} * (${cellValorResidual}/100))) / ${cellHorasUtiles}) * E1`};
+      wsMaquinaria[cellGastoConservacion] = { f: `+${cellPrecioDolar} * E1 * ${cellCoeficiente} `};
+
+      const cellPrecioDolarImpl = `D${excelRowIndex + 1}`;
+      const cellCoeficienteImpl = `E${excelRowIndex + 1}`;
+      const cellHorasUtilesImpl = `F${excelRowIndex + 1}`;
+      const cellValorResidualImpl = `G${excelRowIndex + 1}`;
+      const cellConsumoCombustibleImpl = `H${excelRowIndex + 1}`;
+      const cellAmortizacionImplemento = XLSX.utils.encode_cell({ c: 8, r: rowIndex + 1 });
+      const cellCostoCombustibleImplemento = XLSX.utils.encode_cell({ c: 9, r: rowIndex + 1 });
+      const cellGastoConservacionImplemento = XLSX.utils.encode_cell({ c: 10, r: rowIndex + 1 });
+      const cellCostoEconomico = XLSX.utils.encode_cell({ c: 11, r: rowIndex + 1 });
+
+      wsMaquinaria[cellAmortizacionImplemento] = { f: `+((${cellPrecioDolarImpl} - (${cellPrecioDolarImpl} * (${cellValorResidualImpl}/100))) / ${cellHorasUtilesImpl}) * E1` };
+      wsMaquinaria[cellCostoCombustibleImplemento] = { f: `+C1 * ${cellConsumoCombustibleImpl} * ${cellPotencia}`};
+      wsMaquinaria[cellGastoConservacionImplemento] = { f: `+${cellPrecioDolarImpl} * E1 * ${cellCoeficienteImpl} `};
+      wsMaquinaria[cellCostoEconomico] = { f: `+${cellAmortizacion} + ${cellAmortizacionImplemento} + ${cellCostoCombustibleImplemento} + ${cellGastoConservacion} + ${cellGastoConservacionImplemento}` };
     });
-  });
 
-  //Hoja Maquinaria
-  const wsMaquinaria = XLSX.utils.json_to_sheet(maquinariaRows, { origin: 1 });
+    XLSX.utils.book_append_sheet(workbook, wsMaquinaria, 'Maquinaria');
+  }
 
-  // Cabeceras Gasoil y Dolar
-  wsMaquinaria[`A1`] = { v: 'Gasoil $/litro' };
-  wsMaquinaria[`B1`] = { v: valorGasoilina };
-  wsMaquinaria[`C1`] = { v: 'cotización dólar' };
-  wsMaquinaria[`D1`] = { v: valorDolar };
-
-  const range = XLSX.utils.decode_range(wsMaquinaria['!ref']);
-  range.s.r = 0;
-  wsMaquinaria['!ref'] = XLSX.utils.encode_range(range);
-
-  // Formulas de Maquinaria
-  dataMaquinaria.forEach((plan, index) => {
-    const excelRowIndex = index * 2 + 3; // empieza en fila 2, salta de 2 en 2 por cada plan
-    const rowIndex = excelRowIndex - 1;
-
-    const cellPotencia = `C${excelRowIndex}`;
-    const cellPrecioDolar = `D${excelRowIndex}`;
-    const cellCoeficiente = `E${excelRowIndex}`;
-    const cellHorasUtiles = `F${excelRowIndex}`;
-    const cellValorResidual = `G${excelRowIndex}`;
-    const cellAmortizacion = XLSX.utils.encode_cell({ c: 8, r: rowIndex }); // Fila I
-    const cellGastoConservacion = XLSX.utils.encode_cell({ c: 10, r: rowIndex }); // Fila K 
-    
-    wsMaquinaria[cellAmortizacion] = { f: `+((${cellPrecioDolar} - (${cellPrecioDolar} * (${cellValorResidual}/100))) / ${cellHorasUtiles}) * D1`};
-    wsMaquinaria[cellGastoConservacion] = { f: `+${cellPrecioDolar} * D1 * ${cellCoeficiente} `};
-
-    const cellPrecioDolarImpl = `D${excelRowIndex + 1}`;
-    const cellCoeficienteImpl = `E${excelRowIndex + 1}`;
-    const cellHorasUtilesImpl = `F${excelRowIndex + 1}`;
-    const cellValorResidualImpl = `G${excelRowIndex + 1}`;
-    const cellConsumoCombustibleImpl = `H${excelRowIndex + 1}`;
-    const cellAmortizacionImplemento = XLSX.utils.encode_cell({ c: 8, r: rowIndex + 1 }); // Fila I
-    const cellCostoCombustibleImplemento = XLSX.utils.encode_cell({ c: 9, r: rowIndex + 1 }); // Fila J
-    const cellGastoConservacionImplemento = XLSX.utils.encode_cell({ c: 10, r: rowIndex + 1 }); // Fila K
-    const cellCostoEconomico = XLSX.utils.encode_cell({ c: 11, r: rowIndex + 1 }); // Fila L
-
-    wsMaquinaria[cellAmortizacionImplemento] = { f: `+((${cellPrecioDolarImpl} - (${cellPrecioDolarImpl} * (${cellValorResidualImpl}/100))) / ${cellHorasUtilesImpl}) * D1` };
-    wsMaquinaria[cellCostoCombustibleImplemento] = { f: `+B1 * ${cellConsumoCombustibleImpl} * ${cellPotencia}`};
-    wsMaquinaria[cellGastoConservacionImplemento] = { f: `+${cellPrecioDolarImpl} * D1 * ${cellCoeficienteImpl} `};
-    wsMaquinaria[cellCostoEconomico] = { f: `+${cellAmortizacion} + ${cellAmortizacionImplemento} + ${cellCostoCombustibleImplemento} + ${cellGastoConservacion} + ${cellGastoConservacionImplemento}` };
-
-  });
-
-  // Agrega hoja de maquinaria al libro
-  XLSX.utils.book_append_sheet(workbook, wsMaquinaria, 'Maquinaria');
-
-  // Hoja 2: Fertilizantes
+  // Fertilizantes
   if (dataFertilizantes.length > 0) {
-    const wsFertilizantes = XLSX.utils.json_to_sheet(dataFertilizantes);
+    const fertilizanteRows = [];
+    dataFertilizantes.forEach((plan, index) => {
+      fertilizanteRows.push({
+        'Plan': `${index + 1}`,
+        'Principio Activo': plan.fertilizante?.nombre || '',
+        'Precio envase ($ USD)': plan.fertilizante?.precioEnvaseDolar || '',
+        'Dosis aplicación': plan.fertilizante?.dosisAplicacion || '',
+        'Unidad dosis': plan.fertilizante?.unidadDosisAplicacion || '',
+        'Volumen envase': plan.fertilizante?.volumenEnvase || '',
+        'Unidad volumen': plan.fertilizante?.unidadVolumenEnvase || '',
+        'Cantidad de tratamientos': plan.cantTratamientos || '',
+        'Costo total por tratamiento ($)': '',
+        'Costo total por hectárea ($)': '',
+      });
+    });
+
+    const wsFertilizantes = XLSX.utils.json_to_sheet(fertilizanteRows, { origin: 1 });
+
+    wsFertilizantes[`B1`] = { v: 'Cotización dólar' };
+    wsFertilizantes[`C1`] = { v: valorDolar };
+
+    const rangeFert = XLSX.utils.decode_range(wsFertilizantes['!ref']);
+    rangeFert.s.r = 0;
+    wsFertilizantes['!ref'] = XLSX.utils.encode_range(rangeFert);
+
+    dataFertilizantes.forEach((plan, index) => {
+      const excelRowIndex = index + 3;
+      const rowIndex = excelRowIndex - 1;
+
+      const cellPrecioDolarFertilizante = `C${excelRowIndex}`;
+      const cellDosisHa = `D${excelRowIndex}`;
+      const cellTratamientos = `H${excelRowIndex}`;
+      const cellCostoTotalTratamiento  = XLSX.utils.encode_cell({ c: 8, r: rowIndex });
+      const cellCostoTotalHa = XLSX.utils.encode_cell({ c: 9, r: rowIndex });
+
+      wsFertilizantes[cellCostoTotalTratamiento] = { f: `+${cellPrecioDolarFertilizante} * C1 * ${cellDosisHa} `};
+      wsFertilizantes[cellCostoTotalHa] = { f: `+${cellTratamientos} * ${cellCostoTotalTratamiento} `};
+    });
+
     XLSX.utils.book_append_sheet(workbook, wsFertilizantes, 'Fertilizantes');
   }
 
-  // Hoja 3: Sanitizantes
+  // Sanitizantes
   if (dataSanitizantes.length > 0) {
-    const wsSanitizantes = XLSX.utils.json_to_sheet(dataSanitizantes);
+    const sanitizanteRows = [];
+    dataSanitizantes.forEach((plan, index) => {
+      sanitizanteRows.push({
+        'Insumos': `${index + 1}`,
+        'Principio Activo': plan.sanitizante?.nombre || '',
+        'Tipo': plan.sanitizante?.tipo || '',
+        'Precio envase ($ USD)': plan.sanitizante?.precioEnvaseDolar || '',
+        'Dosis aplicación': plan.sanitizante?.dosisAplicacion || '',
+        'Unidad dosis': plan.sanitizante?.unidadDosisAplicacion || '',
+        'Volumen envase': plan.sanitizante?.volumenEnvase || '',
+        'Unidad volumen': plan.sanitizante?.unidadVolumenEnvase || '',
+        'Volumen por Hectárea': plan.volumenPorHectarea || '',
+        'Cantidad de tratamientos': plan.cantTratamientos || '',
+        'Cantidad': '',
+        'Costo total por tratamiento ($)': '',
+        'Costo total por hectárea ($)': '',
+      });
+    });
+
+    const wsSanitizantes = XLSX.utils.json_to_sheet(sanitizanteRows, { origin: 1 });
+
+    wsSanitizantes[`B1`] = { v: 'Cotización dólar' };
+    wsSanitizantes[`C1`] = { v: valorDolar };
+
+    const rangeSani = XLSX.utils.decode_range(wsSanitizantes['!ref']);
+    rangeSani.s.r = 0;
+    wsSanitizantes['!ref'] = XLSX.utils.encode_range(rangeSani);
+
+    dataSanitizantes.forEach((plan, index) => {
+      const excelRowIndex = index + 3;
+      const rowIndex = excelRowIndex - 1;
+
+      const cellPrecioEnvase = `D${excelRowIndex}`;
+      const cellDosisAplicacion = `E${excelRowIndex}`;
+      const cellVolumenEnvase = `G${excelRowIndex}`;
+      const cellVolumenHa = `I${excelRowIndex}`;
+      const cellTratamientos = `J${excelRowIndex}`;
+      const cellCantidad = XLSX.utils.encode_cell({ c: 10, r: rowIndex });
+      const cellCostoTratamiento = XLSX.utils.encode_cell({ c: 11, r: rowIndex });
+      const cellCostoHa = XLSX.utils.encode_cell({ c: 12, r: rowIndex });
+
+      wsSanitizantes[cellCantidad] = { f: `+${cellDosisAplicacion} * ${cellVolumenHa}` };
+      wsSanitizantes[cellCostoTratamiento] = { f: `+(${cellPrecioEnvase} * C1) * (${cellDosisAplicacion} / ${cellVolumenEnvase})` };
+      wsSanitizantes[cellCostoHa] = { f: `+${cellCostoTratamiento} * ${cellVolumenHa} * ${cellTratamientos}` };
+    });
+
     XLSX.utils.book_append_sheet(workbook, wsSanitizantes, 'Sanitizantes');
   }
 
-  // Exportar archivo
   const wbout = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
   const blob = new Blob([wbout], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
   saveAs(blob, `${fileName}.xlsx`);
 }
-
-// dataMaquinaria ejemplo:
-// {
-//   "id": 1,
-//   "tractor": {
-//       "id": 1,
-//       "nombre": "Tractor 1",
-//       "precioDolar": 60775,
-//       "gastoMantenimiento": 0.00007,
-//       "horasVidaUtil": 12000,
-//       "porcentajeValorResidual": 20,
-//       "potencia": 60
-//   },
-//   "implemento": {
-//       "id": 1,
-//       "nombre": "Acoplado Metálico",
-//       "precioDolar": 8287.5,
-//       "gastoMantenimiento": 0.0004,
-//       "horasVidaUtil": 10000,
-//       "porcentajeValorResidual": 10,
-//       "consumoCombustible": 0.1
-//   },
-//   "amortizacionTractor": 4862,
-//   "gastoConservacionTractor": 5105,
-//   "amortizacionImplemento": 895,
-//   "costoCombustibleImplemento": 0,
-//   "gastoConservacionImplemento": 3978,
-//   "costoEconomico": 14840
-// } 
